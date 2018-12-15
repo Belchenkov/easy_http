24b0e26e3a8922f51963ae42cf72939b59f3e04c
Aleksey Belchenkov <u608110@gmail.com>
2018-12-15 19:27:10 +0300

    Post Method

--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
 const http = new easyHTTP();
-const urlPosts = 'https://jsonplaceholder.typicode.com/posts2';
+const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
 
 // Get Posts
-http.get(urlPosts, function (err, response) {
+/*http.get(urlPosts, function (err, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(response);
+    }
+});*/
+
+// Get Single Post
+/*
+http.get(`${urlPosts}/1`, function (err, response) {
     if (err) {
         console.log(err);
     } else {
         console.log(response);
     }
 });
+*/
 
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post
+http.post(urlPosts, data, function (err, post) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
